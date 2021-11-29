35a40ce78e8b776df7625a9afaff0d33a49bdded
Saikiran1234567 <saikiranboppana888@gmail.com>
2021-11-29 13:09:53 +0530

    SignUp Modification

--- signup.js
+++ signup.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
-app.get('/signup',(req,res) =>{
+app.get('/',(req,res) =>{
     res.send("hello Hippi wecome to voltuswave");
 });
+app.post('/signup',(req,res)=>{
+    const name = req.body.username
+    const email = req.body.useremail
+    const password = req.body.userpswd
+    const checkbox = req.body.usercheck
 
-app.listen(4000);
+    console.log(name,email,password,checkbox);
+});
+
+app.listen(4001,(error)=>{
+    if(error) console.log(error);
+    else console.log("Application Running on port 4001");
+});

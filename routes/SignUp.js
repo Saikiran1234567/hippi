391ada23cbbe554d21a6653803e84006197b423b
Saikiran1234567 <saikiranboppana888@gmail.com>
2021-11-30 10:57:12 +0530

    Updated SignUp Route

--- routes/SignUp.js
+++ routes/SignUp.js
@@ -4,12 +4,28 @@ const Joi = require("joi");
 
 router.post("/SignUp", async(req,res)=>{
     try {
-        const name = req.body.username
-        const email = req.body.useremail
-        const password = req.body.userpswd
-        const checkbox = req.body.usercheck
+        const {error} = validate(req.body);
+        if(error) return res.status(400).send(error.details[0].message);
+        let user ={
+            name : req.body.username,
+            email : req.body.useremail,
+            password : req.body.userpswd,
+            checkbox : req.body.usercheck
+        }
+        res.send(user)
     } catch (error) {
-        console.log(error)
+        return res.send(error);
     }
 })
-module.exports = router
+module.exports = router;
+
+const validate = (requestObj)=>{
+    const schema = Joi.object({
+        username : Joi.string().min(5).required(),
+        useremail : Joi.string().required().email(),
+        userpswd : Joi.string().min(8).required(),
+        usercheck : Joi.boolean().required() 
+
+    });
+    return schema.validate(requestObj)
+}
